919471350e3b9e78d2c8594b58e2621d2c217c9c
mordini <yahewisthedevil@yahoo.com>
2022-03-18 15:23:00 -0400

    added node installer

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 ## Link .vimrc
 ln -s ~/vimrc ~/.vim && echo "source ~/vimrc/.vimrc" > ~/.vimrc
 
+## install node version manager NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+## Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+## install latest node and lts
+nvm install node
+nvm install --lts
+
 ## install fd
 sudo apt install fd-find
 
